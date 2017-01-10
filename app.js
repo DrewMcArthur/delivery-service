d3c993616eeb8ac71e196c7973f739ae58b50395
Drew McArthur <drewmcarthur1@gmail.com>
2017-01-10 14:03:50 -0500

    added mysql support

--- app.js
+++ app.js
@@ -4,6 +4,27 @@
 
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'deliveryapp'
+});
+ 
+connection.connect();
+ 
+/*
+connection.query('SELECT * from < table name >', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+*/
+ 
+
 
 //this hosts the files located in the ./public directory
 app.use(express.static(__dirname + '/public'));
@@ -13,3 +34,5 @@ app.listen(80, function(){
     //callback function, completely optional.   
     console.log("Server is running on port 80");      
 });
+
+connection.end();
