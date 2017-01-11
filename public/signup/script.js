ab1e98604ebfbe475da039d0a5badd92e9f8425b
Drew McArthur <drewmcarthur1@gmail.com>
2017-01-11 15:46:46 +0000

    ready to start sending signup data to server

--- public/signup/script.js
+++ public/signup/script.js
@@ -1,11 +1,15 @@
 var socket = io();
 
 // implement client side socket stuff
-
 $(document).on('ready',function(){
-	$('.user-signup-form > .submit').on('click', function(){
+	$('.submit').on('click', function(){
 		var data = [];
-		var children = $('.user-signup-form').children();
-		console.log(children);
+		var children = $('.user-signup-form :input');
+
+		// get data from form into data array
+		for (var i = 0; i < children.length; i++)
+			data.push($(children[i]).val());
+
+		// do something with the password and send the data to the server
 	});
 });
