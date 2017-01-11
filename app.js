346a29293de6a5ce6c2064d23ee8e36651cfa924
Drew McArthur <drewmcarthur1@gmail.com>
2017-01-11 16:29:09 +0000

    missing = in app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var express = require('express');
 var app = express();
 var mysql = require('mysql');
 var io = require('socketio')(http);
+var bcrypt = require('bcrypt');
+
+// variables
+const saltRounds = 5;
 
 // sql connection info
 var db = mysql.createConnection({
@@ -25,25 +29,35 @@ io.on('connection', function(socket){
 
 	// communications for user signup
 	socket.on('signup', function(data){
-		// sql query to add user with [data] to user table
-		var signupquery = "INSERT INTO user VALUES (";
-		for (datum in data)
-			signupquery += data[datum];
-		signupquery += ");"
-
-		console.log("Adding new user to database with query: ");
-		console.log("	" + signupquery);
-		// add user to user table with [data]
-		db.query(signupquery, function(err, rows, fields) {
-			if (!err) {
-				// set user_id
-				console.log("signup success, printing rows then fields");
-				console.log(rows);
-				console.log(fields);
-				socket.emit('signupsuccess');
+		bcrypt.hash(data[1], saltRounds, function(err, hash) {
+			if (err) {
+				console.log("Error hashing password: " + err);
+				socket.emit('signuperror', "Error hashing password: " + err);
 			} else {
-				// if there was an error signing the user up, forward the error to the client
-				socket.emit('signuperror', err);
+				data[1] = hash;
+
+				// sql query to add user with [data] to user table
+				var signupquery = "INSERT INTO user VALUES (";
+				for (datum in data) {
+					signupquery += data[datum];
+					signupquery += (i + 1 == data.length ? ");" : ", ");
+				}
+
+				console.log("Adding new user to database with query: ");
+				console.log("	" + signupquery);
+				// add user to user table with [data]
+				db.query(signupquery, function(err, rows, fields) {
+					if (!err) {
+						// set user_id
+						console.log("signup success, printing rows then fields");
+						console.log(rows);
+						console.log(fields);
+						socket.emit('signupsuccess');
+					} else {
+						// if there was an error signing the user up, forward the error to the client
+						socket.emit('signuperror', "Database Error: " + err);
+					}
+				});
 			}
 		});
 	});
