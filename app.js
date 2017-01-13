62f959838a7a28ddfc2841ad32d06ab0c03b237e
Drew McArthur <drewmcarthur1@gmail.com>
2017-01-13 00:35:31 +0000

    login works!?!?

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var http = require('http').Server(app);
 var mysql = require('mysql');
 var io = require('socket.io')(http);
 var bcrypt = require('bcrypt');
+var bodyParser = require('body-parser');
 
 app.use(session({
   secret: 'keyboard doggo pupper',
@@ -19,6 +20,11 @@ app.use(session({
   cookie: { secure: false } // change this once i get HTTPS running
 }));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 // variables
 const saltRounds = 5;
@@ -32,90 +38,87 @@ var db = mysql.createConnection({
 });
 db.connect();
 
-// defining socket.io connection
-io.on('connection', function(socket){
-	
-	logger("Connection established: ");
-	logger("    " + socket);
+// communications for user signup
+app.post('/signup', function(req, res){
+	var data = req.body;
+	bcrypt.hash(data.password, saltRounds, function(err, hash) {
+		if (err) {
+			logger("Error hashing password: " + err); 
+			res.status(500).send(err);
+		} else {
+			data.password = hash;
 
-	// user's id, defined when the user signs up or logs in.
-	req.session.user_id = 0;
+			// sql query to add user with [data] to user table
+			var signupquery = "INSERT INTO user(username, password, email, phone, room) VALUES (";
+			for (var i in data) {
+				signupquery += "'" + data[i] + "'";
+				signupquery += (i == data.length - 1 ? ");" : ", ");
+			}
 
-	// communications for user signup
-	socket.on('signup', function(data){
-		bcrypt.hash(data[1], saltRounds, function(err, hash) {
-			if (err) {
-				logger("Error hashing password: " + err); socket.emit('signuperror', "Error hashing password: " + err);
-			} else {
-				data[1] = hash;
-
-				// sql query to add user with [data] to user table
-				var signupquery = "INSERT INTO user(username, password, email, phone, room) VALUES (";
-				for (var i in data) {
-					signupquery += "'" + data[i] + "'";
-					signupquery += (i == data.length - 1 ? ");" : ", ");
+			logger("Adding new user to database with query: ");
+			logger("	" + signupquery);
+			// add user to user table with [data]
+			db.query(signupquery, function(err, rows) {
+				if (!err) {
+					// set user_id
+					req.session.user_id = rows.insertId;
+					logger("Signup Success: ");
+					logger(rows.insertId);
+					res.redirect('/');
+				} else {
+					// if there was an error signing the user up, forward the error to the client
+					logger("Signup Error: Database Error: " + err);
+					res.status(500).send(err);
 				}
-
-				logger("Adding new user to database with query: ");
-				logger("	" + signupquery);
-				// add user to user table with [data]
-				db.query(signupquery, function(err, rows) {
-					if (!err) {
-						// set user_id
-						req.session.user_id = rows.insertId;
-						logger("Signup Success: ");
-						logger(rows.insertId);
-						socket.emit('signupsuccess', rows.insertId);
-					} else {
-						// if there was an error signing the user up, forward the error to the client
-						logger("Signup Error: Database Error: " + err);
-						socket.emit('signuperror', "Database Error: " + err);
-					}
-				});
-			}
-		});
+			});
+		}
 	});
-	socket.on('login', function(data){
-		// get hashed password from database
-		var q = "SELECT id,password FROM user WHERE email='" + data[0] + "';";
-		db.query(q, function(err, rows){
-			if (err) {
-				logger("Login Error: ");
-				logger("    " + err);
-				socket.emit("loginerror", err.toString());
-			} else if (rows.length == 0) {
-				// no email exists
-				logger("Login Error: Invalid Email");
-				socket.emit('loginerror', 'invalidcredentials');
-			} else { 
-				var hash = rows[0].password;
-				var id = rows[0].id;
-				// check if thats the right password
-				bcrypt.compare(data[1], hash, function(err, res){
-					if (err) {
-						logger("Login Error: ");
-						logger("    " + err);
-						socket.emit('loginerror', err.toString());
-					} else if (!res) {
-						logger("Login Error: Incorrect Password");
-						socket.emit('loginerror', "invalidcredentials");
-					} else {
-						logger("User " + id + " logged in.");
-						req.session.user_id = id;
-						socket.emit('loginsuccess', id);
-					}
-				});
-			}
-		});
+});
+app.post('/login', function(req, res){
+	var email = req.body.email;
+	var pass = req.body.password;
+	// get hashed password from database
+	var q = "SELECT id,password FROM user WHERE email='" + email + "';";
+	db.query(q, function(err, rows){
+		if (err) {
+			logger("Login Error: ");
+			logger("    " + err);
+			res.status(500).send(err);
+		} else if (rows.length == 0) {
+			// no email exists
+			logger("Login Error: Invalid Email");
+			res.send({
+				success: false,
+				msg: "Invalid credentials."
+			});
+		} else { 
+			var hash = rows[0].password;
+			var id = rows[0].id;
+			// check if thats the right password
+			bcrypt.compare(pass, hash, function(err, success){
+				if (err) {
+					logger("Login Error: ");
+					logger("    " + err);
+					res.status(500).send(err);
+				} else if (!success) {
+					logger("Login Error: Incorrect Password");
+					res.send({
+						success: false,
+						msg: "Invalid credentials."
+					});
+				} else {
+					logger("User " + id + " logged in.");
+					req.session.user_id = id;
+					res.redirect('/');
+				}
+			});
+		}
 	});
-	socket.on('logout', function() {
-		logger("User " + id + " logged out.");
-		req.session.user_id = 0;
-		socket.emit('logoutsuccess');
-	});
-	socket.on('disconnect', function() {
-		logger("User " + id + " disconnected.");
-	});
+});
+app.get('/logout', function(req, res) {
+	logger("User " + id + " logged out.");
+	req.session.user_id = 0;
+	res.redirect('/');
 });
 
 // functions
@@ -130,13 +133,6 @@ function logger(message){ //log to the console and a hard file
 	);
 }
  
-// routes
-/* don't think i need this because i can just put another index inside /public/signup/ for the same effect
-app.get("/signup", function(req,res){
-	res.render("/public/signup.html");
-});
-*/
-
 //this hosts the files located in the ./public directory
 app.use(express.static(__dirname + '/public'));
 
